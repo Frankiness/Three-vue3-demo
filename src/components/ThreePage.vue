2514fc3ec1d1fbb9c9bd6010bb31d02d7ba9e530
tanyulin <690626310@qq.com>
2022-05-05 17:40:59 +0800

    feat:平滑过渡镜头

--- src/components/ThreePage.vue
+++ src/components/ThreePage.vue
@@ -1,12 +1,22 @@
 <template>
-  <div id="map"></div>
 </template>
+<!--suppress JSVoidFunctionReturnValueUsed -->
 <script setup>
-import * as THREE from 'three'
 import RendererTemplate from "../utils/RendererTemplate";
+import {GLTFLoader} from "three/examples/jsm/loaders/GLTFLoader";
 
 const template = new RendererTemplate()
 template.init()
+
+const loadBuildingModel = () => {
+  const GLTF = new GLTFLoader().load('model/leishen.glb', (gltf) => {
+    const model = gltf.scene
+    model.scale.set(0.1, 0.1, 0.1)
+    template.scene.add(gltf.scene);
+  })
+}
+
+loadBuildingModel()
 </script>
 
 <style scoped>
--- src/utils/RendererTemplate.js
+++ src/utils/RendererTemplate.js
@@ -1,5 +1,6 @@
 import * as THREE from 'three'
 import {OrbitControls} from "three/examples/jsm/controls/OrbitControls";
+import {TWEEN} from "three/examples/jsm/libs/tween.module.min";
 
 export default class RendererTemplate {
   constructor() {
@@ -9,13 +10,15 @@ export default class RendererTemplate {
       fov: 45,
       aspect: window.innerWidth / window.innerHeight,
       near: 1,
-      far: 1000,
+      far: 100000,
     };
     this.cameraPostion = new THREE.Vector3(10, 10, 10);
     this.cameraLookAt = new THREE.Vector3(0, 0, 0);
     this.rendererColor = new THREE.Color(0xF0F8FF);
     this.rendererWidth = window.innerWidth;
     this.rendererHeight = window.innerHeight;
+
+    this.p1 = {x: 10, y: 20, z: 100}
   }
 
   initPerspectiveCamera() {
@@ -45,30 +48,102 @@ export default class RendererTemplate {
     renderer.setSize(this.rendererWidth, this.rendererHeight);
     this.el.appendChild(renderer.domElement);
     this.renderer = renderer;
-    const controls = new OrbitControls(this.camera, renderer.domElement)//轨迹控制
+    this.controls = new OrbitControls(this.camera, renderer.domElement)//轨迹控制
   }
 
   init() {
     this.initScene();
     this.initPerspectiveCamera();
     this.initRenderer();
-    const animate = () => {
-      requestAnimationFrame(animate);
-      this.renderer.render( this.scene, this.camera );
-    }
-    animate()
     this.initFloor()
     this.addAxes()
+    this.initLight()
+    const animate = () => {
+      requestAnimationFrame(animate);
+      this.renderer.render(this.scene, this.camera);
+      try {　// 放在 TWEEN.js未加载完成导致报错
+        TWEEN.update();
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    animate()
+    this.flyTo()
+    this.randomBoxGeometry()
+
   }
-  initFloor(){
-    const floorGeometry = new THREE.PlaneGeometry(10,10,1)
-    const floorMaterial = new THREE.MeshBasicMaterial({color:0x696969})
-    const floor = new THREE.Mesh(floorGeometry,floorMaterial)
-    floor.rotation.x = -Math.PI/2
+
+  initFloor() {
+    const floorGeometry = new THREE.PlaneGeometry(1000, 1000, 1)
+    const floorMaterial = new THREE.ShadowMaterial({color: 0x000000, opacity: 0.2});
+    const floor = new THREE.Mesh(floorGeometry, floorMaterial)
+    floor.rotation.x = -Math.PI / 2
+    floor.position.y = -0.01
     this.scene.add(floor)
   }
-  addAxes(){
+
+  initLight() {
+    const ambientLight = new THREE.AmbientLight(0xffffff); // soft white light
+    this.scene.add(ambientLight);
+    const hemisphere = new THREE.HemisphereLight(0xffffbb, 0xffffff, 1);
+    this.scene.add(hemisphere);
+  }
+
+  addAxes() {
     const axes = new THREE.AxisHelper(100);
     this.scene.add(axes);
+
+    const helper = new THREE.GridHelper(2000, 100);
+    helper.material.opacity = 0.25;
+    helper.material.transparent = true;
+    this.scene.add(helper);
   }
+
+  flyTo() {
+    let tweenA = this.cameraCon({x: 10, y: 50, z: 50}, 3000)
+    let tweenB = this.cameraCon({x: 50, y: 25, z: -50}, 4000)
+    tweenA.chain(tweenB)
+    tweenB.start()
+  }
+
+  cameraCon(p2, time) {
+    let tween = new TWEEN.Tween(this.p1).to(p2, time).easing(TWEEN.Easing.Quadratic.InOut)
+    tween.onUpdate(() => {
+      this.camera.position.set(this.p1.x, this.p1.y, this.p1.z)
+      this.camera.lookAt(0, 0, 0)
+      this.controls.target.set(0, 0, 0) // 确保镜头移动后，视觉中心还在圆点处
+      this.controls.update()
+    })
+    return tween
+  }
+
+  randomBoxGeometry() {
+    for (let i = 0; i < 30; i++) {
+      let g = new THREE.BoxGeometry(1, 1, 1)
+      let m = new THREE.MeshBasicMaterial({color: 0x000fff})
+      let box = new THREE.Mesh(g, m)
+      box.position.set(Math.random() * 10, Math.random() * 10, Math.random() * 10)
+      this.scene.add(box)
+    }
+
+    const container = document.querySelector("body")
+    container.addEventListener('click', (e) => {
+      this.addRaycaster(e)
+    })
+  }
+
+
+  addRaycaster(event) {
+    let vector = new THREE.Vector3((event.clientX / window.innerWidth) * 2 - 1, -(event.clientY / window
+        .innerHeight) * 2 + 1, 0.5);
+    vector = vector.unproject(this.camera); // 将屏幕的坐标转换成三维场景中的坐标
+    const meshArray = this.scene.children.filter(item => item.isMesh)
+    const raycaster = new THREE.Raycaster(this.camera.position, vector.sub(this.camera.position).normalize());
+    const intersects = raycaster.intersectObjects(meshArray, true);
+    if (intersects.length > 0) {
+      intersects[0].object.material.color.set("#ff0000");
+    }
+  }
+
+
 }
