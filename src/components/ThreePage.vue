6bf666bf4c4ab8c3acc65cce968b94c6743351af
tanyulin <690626310@qq.com>
2022-06-24 15:45:04 +0800

    fix:删除多余文件

--- src/components/ThreePage.vue
+++ src/components/ThreePage.vue
@@ -4,7 +4,6 @@
 <!--suppress JSVoidFunctionReturnValueUsed -->
 <script setup>
 import RendererTemplate from "../utils/RendererTemplate";
-import {GLTFLoader} from "three/examples/jsm/loaders/GLTFLoader";
 import * as THREE from "three";
 import * as d3 from "d3";
 import {onMounted, ref} from "vue";
@@ -83,28 +82,6 @@ onMounted(() => {
   const template = new RendererTemplate(container.value);
   template.init();
 
-  const loadBuildingModel = () => {
-    const GLTF = new GLTFLoader().load(
-        "model/leishen.glb",
-        (gltf) => {
-          const model = gltf.scene;
-          model.scale.set(0.1, 0.1, 0.1);
-          model.position.set(1, 1, 0);
-          model.rotation.x = new THREE.Euler(0, 1, 1.57, "XYZ");
-          console.log(model);
-          template.scene.add(gltf.scene);
-        },
-        // 加载过程中的回调函数
-        (xhr) => {
-          console.log((xhr.loaded / xhr.total) * 100 + "% loaded");
-        },
-        // 加载出错的回调
-        (err) => {
-          console.error("An error happened", err);
-        }
-    );
-  };
-  loadBuildingModel()
 })
 </script>
 
