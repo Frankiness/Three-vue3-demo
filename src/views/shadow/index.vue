1584b94ea789c190cc6d9ee4fadafcc5973fd574
tanyulin <690626310@qq.com>
2022-10-17 15:28:19 +0800

    gltf模型阴影

--- src/views/shadow/index.vue
+++ src/views/shadow/index.vue
@@ -7,13 +7,15 @@ import * as THREE from "three";
 import { onMounted } from "vue";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 import { ref } from "vue";
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 
 let shadowContainer = ref(null);
 const init = () => {
   const scene = new THREE.Scene();
-  const camera = new THREE.PerspectiveCamera(45, 16 / 9, 1, 10000, 1);
+  scene.background = 0xffffff
+  const camera = new THREE.PerspectiveCamera(45, 16 / 9, 1, 1000, 1);
   const geometry = new THREE.BoxGeometry(1, 1, 1); //代表x,y,z三个方向的值
-  const material = new THREE.MeshBasicMaterial({ color: 0x000000 }); //指定立方体的材质、颜色...
+  const material = new THREE.MeshBasicMaterial({ color: 0xdddddd }); //指定立方体的材质、颜色...
   const cube = new THREE.Mesh(geometry, material); //通过Mesh（网格）将立方体与材质融合
   cube.position.set(0, 5, 0);
   cube.castShadow = true;
@@ -28,7 +30,7 @@ const init = () => {
 
   const groundGeometry = new THREE.PlaneGeometry(50, 50);
   const groundMaterial = new THREE.MeshPhongMaterial({
-    color: 0xcc8866,
+    color: 0xffffff,
     side: THREE.DoubleSide,
   });
   const groundMesh = new THREE.Mesh(groundGeometry, groundMaterial);
@@ -37,13 +39,26 @@ const init = () => {
   scene.add(groundMesh);
 
   let spotLight = new THREE.SpotLight(0xffffff);
-  spotLight.position.set(10, 80, 10);
+  spotLight.position.set(100, 80, 10);
   spotLight.castShadow = true;
   spotLight.shadow.mapSize.width = 2048 * 2; //阴影贴图宽度设置为2048像素（数值过大会卡顿）
   spotLight.shadow.mapSize.height = 2048 * 2; //阴影贴图高度设置为2048像素
-  const spotLightHelper = new THREE.SpotLightHelper(spotLight, 50, 0xff0000);
+  const spotLightHelper = new THREE.SpotLightHelper(spotLight, 50, 0xfff000);
   scene.add(spotLightHelper);
   scene.add(spotLight);
+
+  const gltfLoader = new GLTFLoader()
+  gltfLoader.load('model/Audi/scene.gltf',(gltf)=>{
+    gltf.scene.scale.set(0.01, 0.01, 0.01);
+    gltf.scene.position.y = 1;
+    gltf.scene.traverse(function (node) {
+      if (node.isMesh) {
+        node.castShadow = true;
+        node.receiveShadow = true
+      }
+    });
+    scene.add(gltf.scene)
+  })
 
   function animate() {
     cube.rotation.x += 0.01;
